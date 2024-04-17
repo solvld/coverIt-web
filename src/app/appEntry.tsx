d707ae09b1dcf433fa2276e36b873ce681a77391
solvld <sol.vld@yandex.ru>
2024-04-17 20:59:06 +0400

    add Axios and Tanstack Query

--- src/app/appEntry.tsx
+++ src/app/appEntry.tsx
@@ -2,10 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from 'app/routes/Routes'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import 'app/styles/index.scss'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
