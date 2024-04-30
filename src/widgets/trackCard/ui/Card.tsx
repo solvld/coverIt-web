df640748349a183fe3df9febd8ee70028f1d1604
solvld <sol.vld@yandex.ru>
2024-04-30 14:03:51 +0400

    feat: add generetad track card layout

---
+++ src/widgets/trackCard/ui/Card.tsx
@@ -0,0 +1,121 @@
+import { trackCoverRes } from 'shared/mocks/trackCoverResponse'
+import styled from 'styled-components'
+import Reload from 'shared/assets/images/reload.svg?react'
+import Download from 'shared/assets/images/download.svg?react'
+import Edit from 'shared/assets/images/edit-image.svg?react'
+import { mood, style } from 'shared/mocks/formSelectors'
+
+const SCard = styled.section`
+  max-width: 75rem;
+  width: 100%;
+  height: 42rem;
+  margin-bottom: 2rem;
+  border-radius: 9px;
+  box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.25);
+  display: grid;
+  grid-template: 1fr 1fr / 32rem 1fr;
+  gap: 2rem;
+  padding: 4rem 0 2rem 4rem;
+`
+const Cover = styled.figure`
+  width: 32rem;
+  height: 32rem;
+
+  img {
+    width: 32rem;
+    height: 32rem;
+  }
+`
+const TrackTitle = styled.h3`
+  font-size: 2rem;
+  padding-top: 0.5rem;
+  padding-bottom: 1.5rem;
+`
+const Description = styled.div`
+  display: flex;
+  height: 28.5rem;
+  flex-direction: column;
+  justify-content: space-between;
+  cursor: default;
+
+  h4 {
+    font-size: 1.5rem;
+    font-weight: 500;
+    margin-bottom: 0.75rem;
+  }
+  p {
+    font-size: 1.25rem;
+  }
+`
+const Tag = styled.span`
+  padding: 4px 10px;
+  color: #fff;
+  border-radius: 9px;
+  background-color: var(--primary-color);
+`
+const TagWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+`
+const Actions = styled.div`
+  display: flex;
+  gap: 2rem;
+  svg {
+    cursor: pointer;
+  }
+`
+const Card = () => {
+  const res = trackCoverRes
+  return (
+    <SCard>
+      <Cover>
+        <img src={res.cover.link} alt="" />
+      </Cover>
+      <div>
+        <TrackTitle>{res.title}</TrackTitle>
+        <Description>
+          <div>
+            <h4>Mood</h4>
+            <TagWrapper>
+              {mood.slice(2, 9).map((e, index) => (
+                <Tag key={index}>{e}</Tag>
+              ))}
+            </TagWrapper>
+          </div>
+
+          <div>
+            <h4>Object / action</h4>
+            <p>wet stone covered with moss, glows a little</p>
+          </div>
+
+          <div>
+            <h4>Surrounding</h4>
+            <p>forest edge, the sun breaks through the bark of the trees</p>
+          </div>
+
+          <div>
+            <h4>Style</h4>
+            <TagWrapper>
+              {style.slice(3, 9).map((e, index) => (
+                <Tag key={index}>{e}</Tag>
+              ))}
+            </TagWrapper>
+          </div>
+        </Description>
+      </div>
+
+      <Actions>
+        <Reload style={{ stroke: 'var(--primary-color)' }} />
+
+        <Edit />
+
+        <a href={res.cover.link} download={`${res.title}.jpeg`}>
+          <Download />
+        </a>
+      </Actions>
+    </SCard>
+  )
+}
+
+export default Card
