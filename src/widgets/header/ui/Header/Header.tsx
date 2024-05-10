e30adc9c9a59f5497d3663b6390b173880af0b49
solvld <sol.vld@yandex.ru>
2024-05-10 20:34:29 +0400

    refactor: update Header by new design

--- src/widgets/header/ui/Header/Header.tsx
+++ src/widgets/header/ui/Header/Header.tsx
@@ -1,32 +1,57 @@
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import Logo from 'shared/assets/images/logo.svg?react'
-import Profile from 'shared/assets/images/user.svg?react'
 
 import s from './styles.module.scss'
 import { useState } from 'react'
+import { useCurrentUser } from 'shared/services/queries'
 
 const Header = () => {
   const [isLoggedIn] = useState(localStorage.getItem('token'))
-  const guardLink = isLoggedIn ? '/profile' : '/sign-in'
+
+  const { data, isSuccess } = useCurrentUser()
 
   const linkStyle = ({ isActive }: { isActive: boolean }) =>
     isActive ? s.activeNavLink : s.navLink
   return (
     <header className={s.header}>
       <nav>
+        <NavLink to={'/'}>
+          <Logo />
+        </NavLink>
         <NavLink to={'/generate'} className={linkStyle}>
           generate
-        </NavLink>
-        <NavLink to={'/'}>
-          <Logo className={s.logo} />
         </NavLink>
         <NavLink to={'/archive'} className={linkStyle}>
           archive
         </NavLink>
+        {isLoggedIn && (
+          <>
+            <NavLink to={'/liked'} className={linkStyle}>
+              liked playlists
+            </NavLink>
+            <NavLink to={'/my-playlist'} className={linkStyle}>
+              my playlists
+            </NavLink>
+            <NavLink to={'/find-user'} className={linkStyle}>
+              find users
+            </NavLink>
+          </>
+        )}
       </nav>
-      <NavLink to={guardLink}>
-        <Profile className={s.profileLink} />
-      </NavLink>
+
+      <div className={s.profile}>
+        {isLoggedIn ? (
+          <>
+            <Link to={'/'}>subscribe</Link>
+            {isSuccess && <span>{`@${data.username}`}</span>}
+          </>
+        ) : (
+          <>
+            <Link to={'/sign-up'}>sign un</Link>
+            <Link to={'/sign-in'}>log in</Link>
+          </>
+        )}
+      </div>
     </header>
   )
 }
