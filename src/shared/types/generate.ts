404c19e103a090d59fdbcb403264cfff7ca682d8
solvld <sol.vld@yandex.ru>
2024-05-18 15:36:44 +0400

    add generate playlist response type and edit songs mock

--- src/shared/types/generate.ts
+++ src/shared/types/generate.ts
@@ -15,3 +15,43 @@ export interface TrackBody {
   coverDescription: string[]
   isLoFi: boolean
 }
+
+export type Vibe =
+  | 'DANCING_FLOOR'
+  | 'NATURE_DOES_NOT_CARE'
+  | 'BREAKING_DOWN'
+  | 'CAMPFIRE_CALMNESS'
+  | 'TOUGH_AND_STRAIGHT'
+  | 'ENDLESS_JOY'
+
+export interface PlaylistResponse {
+  id: number
+  title: string
+  url: string
+  vibe: Vibe
+  isPrivate: boolean
+  isSaved: boolean
+  author: {
+    id: number
+    username: string
+    email: string
+    loFiPlaylistGenerations: number
+    hiFiPlaylistGenerations: number
+  }
+  tracks: [
+    {
+      title: string
+      authors: string
+    },
+  ]
+  cover: {
+    id: number
+    created: string
+    link: string
+    prompt: string
+    isAbstract: boolean
+    isLoFi: boolean
+  }
+  hiFiGenerationsLeft: number
+  loFiGenerationsLeft: number
+}
