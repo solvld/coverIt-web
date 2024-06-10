555cffed0dec3587a1ad0b29f34fe63549596ec2
LittleCreamSoda <nikitalahin@gmail.com>
2024-06-10 14:03:38 +0400

    setup drag in CoverCard

--- src/entities/coverCard/ui/CoverCard/CoverCard.tsx
+++ src/entities/coverCard/ui/CoverCard/CoverCard.tsx
@@ -1,15 +1,23 @@
+import { useRef } from 'react'
+import styled, { css } from 'styled-components'
+import Draggable from 'react-draggable'
+
 import s from './styles.module.scss'
+
 import CoverImage from './CoverImage'
 import CoverDescription from './CoverDescription'
-import styled from 'styled-components'
+import { getREMValue } from 'shared/utils/getREMValue.ts'
 
-const Card = styled.article<{ $position: { axesX: number; axesY: number } }>`
-  transform: ${props =>
-    `translate(${props.$position.axesX}rem, ${props.$position.axesY}rem)`};
-  @media (max-width: 1376px) {
-    transform: translate(0, 0);
-  }
-`
+const Card = styled.article<{ $position: { axesX: number; axesY: number } }>(
+  ({ $position: { axesX, axesY } }) => css`
+    cursor: grab;
+    // transform: translate(${axesX}rem, ${axesY}rem);
+
+    //@media (max-width: 1376px) {
+    //  transform: translate(0, 0);
+    //}
+  `,
+)
 
 const CoverCard = ({
   title,
@@ -23,11 +31,22 @@ const CoverCard = ({
   index: number
   position: { axesX: number; axesY: number }
 }) => {
+  const cardRef = useRef<HTMLDivElement>(null)
+  const remValue = getREMValue()
+
   return (
-    <Card $position={position} className={s.cardItem}>
-      <CoverImage image={image} />
-      <CoverDescription title={title} songs={songs} />
-    </Card>
+    <Draggable
+      nodeRef={cardRef}
+      defaultPosition={{
+        x: remValue * position.axesX,
+        y: remValue * position.axesY,
+      }}
+    >
+      <Card $position={position} className={s.cardItem} ref={cardRef}>
+        <CoverImage image={image} />
+        <CoverDescription title={title} songs={songs} />
+      </Card>
+    </Draggable>
   )
 }
 
